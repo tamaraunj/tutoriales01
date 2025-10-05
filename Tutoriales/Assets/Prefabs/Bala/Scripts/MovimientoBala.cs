79a1f09040fcf1d66a01d7157156c81e4120609a
tamara m <45763997@fi.unju.edu.ar>
2025-10-05 18:27:25 -0300

    Se crea dos cañones y programar limites o excepciones
    Se crea dos cañones y la logica necesaria para las balas: si la bala sale de la escena o impacta con el jugador, la bala se destruye

--- Tutoriales/Assets/Prefabs/Bala/Scripts/MovimientoBala.cs
+++ Tutoriales/Assets/Prefabs/Bala/Scripts/MovimientoBala.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class MovimientoBala : MonoBehaviour
 {
+    private float limiteEnX = 15f;
+    private float velocidadBala = 4f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,22 @@ public class MovimientoBala : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(4f * Time.deltaTime, 0 ,0); 
+        //Mover la bala en el eje x con una velocidad determinada
+        transform.Translate(velocidadBala * Time.deltaTime, 0 ,0);
+
+        //Destruir la bala si se sale de la escena
+        if (transform.position.x > limiteEnX || transform.position.x < -limiteEnX)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("La bala impactó al jugador");
+            Destroy(gameObject);
+            
+        }
     }
 }
